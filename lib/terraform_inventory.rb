1c2321eb07ce361e6db3912c98fd07b9d9dce608
Tyler Cross <tcross@bandwidth.com>
2014-08-25 11:15:05 -0400

    Autoload CLI and bump version

--- lib/terraform_inventory.rb
+++ lib/terraform_inventory.rb
@@ -2,4 +2,5 @@ require "terraform_inventory/version"
 
 module TerraformInventory
   autoload :TerraformState, "terraform_inventory/terraform_state"
+  autoload :Cli,            "terraform_inventory/cli"
 end
--- lib/terraform_inventory/version.rb
+++ lib/terraform_inventory/version.rb
@@ -1,3 +1,3 @@
 module TerraformInventory
-  VERSION = "0.1.5"
+  VERSION = "0.1.6"
 end
